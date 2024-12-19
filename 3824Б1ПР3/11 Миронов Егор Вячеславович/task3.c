ef429b63f16f96b6c9433f9968a336affcf5dc0e
Egor <122093200+itsumiq@users.noreply.github.com>
2024-12-19 10:38:59 +0300

    Update task3.c

--- 3824Б1ПР3/11 Миронов Егор Вячеславович/task3.c
+++ 3824Б1ПР3/11 Миронов Егор Вячеславович/task3.c
@@ -6,7 +6,32 @@
 // Пример: s1 = "aaa", N1 = 4, s2 = "bbb", N2 = 4
 //   Ответ: "aaabbb"
 // Функции стандартной библиотеки языка С не применять.
+#include <stdlib.h>
+
 char* task3(char *s1, unsigned N1, char *s2, unsigned N2)
 {
-  return "task3";
+  unsigned len1 = 0, len2 = 0;
+
+    while (s1[len1] != '\0') len1++;
+    while (s2[len2] != '\0') len2++;
+
+    unsigned copy1 = (N1 < len1) ? N1 : len1;
+    unsigned copy2 = (N2 < len2) ? N2 : len2;
+
+    char *result = (char *)malloc(copy1 + copy2 + 1);
+    if (!result) {
+        return NULL;
+    }
+
+    for (unsigned i = 0; i < copy1; i++) {
+        result[i] = s1[i];
+    }
+
+    for (unsigned i = 0; i < copy2; i++) {
+        result[copy1 + i] = s2[len2 - copy2 + i];
+    }
+
+    result[copy1 + copy2] = '\0';
+
+    return result;
 }
