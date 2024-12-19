86f6d99125a7cc04cc0aaea8d70dcc4bd13a50f2
Egor <122093200+itsumiq@users.noreply.github.com>
2024-12-19 10:38:14 +0300

    Update task1.c

--- 3824Б1ПР3/11 Миронов Егор Вячеславович/task1.c
+++ 3824Б1ПР3/11 Миронов Егор Вячеславович/task1.c
@@ -1,7 +1,17 @@
 // Задача 1. Даны числа a, b (0 < a < b) и непустой массив A из N элементов.
 // Найти минимальный из тех элементов A, которые содержатся в интервале (a, b).
 // Вернуть 0, если требуемые элементы отсутствуют.
+#include <stddef.h>
+
 int task1(unsigned A[], size_t n, unsigned a, unsigned b)
 {
-  return -1;
+  unsigned min = (unsigned)-1;
+
+  for (size_t i = 0; i < n; i++) {
+    if (A[i] > a && A[i] < b && A[i] < min) {
+      min = A[i];
+    }
+  }
+
+  return min == (unsigned)-1 ? 0 : min;
 }
